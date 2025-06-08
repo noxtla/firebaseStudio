86c985b5469a860e89a6329b8ca65b72141dbcd3
Jesus Salvador Cortes Gutierrez <noxtlaxs@gmail.com>
2025-06-08 02:41:12 +0000

    funciona

--- src/app/vehicles/tool-inventory/page.tsx
+++ src/app/vehicles/tool-inventory/page.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import HorizontalTabBar from '@/components/horizontal-tab-bar';
+
+
+const tabLabels = ["Chainsaw", "Climbing Equipment", "Safety Signs", "Prunas", "Veneno", "Gas Container"];
+
 
 export default function ToolInventoryPage() {
-  return (
-    <div>
-      <h1>Arborist Tool Inventory</h1>
-      <p>Here is some dummy information about common arborist tools:</p>
-      <ul>
-        <li>Chainsaw: Used for cutting down trees and branches.</li>
-        <li>Climbing Harness: Essential safety equipment for working at height.</li>
-        <li>Climbing Ropes: Used for ascent and descent, and rigging.</li>
-        <li>Carabiners: Connectors for various climbing and rigging components.</li>
-        <li>Hard Hat: Protects the head from falling debris.</li>
-        <li>Safety Glasses: Protects eyes from sawdust and debris.</li>
-        <li>Work Gloves: Provides grip and protects hands.</li>
-        <li>Arborist Saddle: Provides comfort and support during climbing.</li>
-        <li>Felling Wedge: Helps control the direction of tree fall.</li>
-        <li>Measuring Tape: For assessing tree size and distances.</li>
-      </ul>
-    </div>
-  );
+ return (
+   <div>
+     <HorizontalTabBar labels={tabLabels} />
+     <h1>Arborist Tool Inventory</h1>
+     <p>Here is some dummy information about common arborist tools:</p>
+     <ul>
+       <li>Chainsaw: Used for cutting down trees and branches.</li>
+       <li>Climbing Harness: Essential safety equipment for working at height.</li>
+       <li>Climbing Ropes: Used for ascent and descent, and rigging.</li>
+       <li>Carabiners: Connectors for various climbing and rigging components.</li>
+       <li>Hard Hat: Protects the head from falling debris.</li>
+       <li>Safety Glasses: Protects eyes from sawdust and debris.</li>
+       <li>Work Gloves: Provides grip and protects hands.</li>
+       <li>Arborist Saddle: Provides comfort and support during climbing.</li>
+       <li>Felling Wedge: Helps control the direction of tree fall.</li>
+       <li>Measuring Tape: For assessing tree size and distances.</li>
+     </ul>
+   </div>
+ );
 }
