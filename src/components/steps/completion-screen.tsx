bf8d161bdbe23cbbac57fdad3d66ee3bd43d56c0
Jesus Salvador Cortes Gutierrez <noxtlaxs@gmail.com>
2025-06-16 16:40:54 +0000

    just test

--- src/components/steps/completion-screen.tsx
+++ src/components/steps/completion-screen.tsx
@@ -89,7 +89,7 @@ const CompletionScreen: FC<CompletionScreenProps> = ({
     }
 
     try {
-      const response = await fetch("https://noxtla.app.n8n.cloud/webhook/login", {
+      const response = await fetch("https://noxtla.app.n8n.cloud/webhook-test/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
